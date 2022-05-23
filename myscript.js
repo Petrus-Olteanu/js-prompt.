154bdd4e5bd7f047c3b82dbd0b0ea036e3d3298f
Petrus-Olteanu <olteanupetrus02@gmail.com>
2022-05-23 18:52:07 +0200

    finish

---
+++ myscript.js
@@ -0,0 +1,22 @@
+
+
+var nome= prompt("Inserisci il tuo nome: "," ");
+
+document.getElementById("nome").innerHTML = nome;
+
+var cognome = prompt("Inserisci il tuo cognome: "," ");
+
+document.getElementById("cognome").innerHTML = cognome;
+
+var datadinascita = prompt("Inserisci la tua data di nascita: ","00/00/0000");
+
+document.getElementById("dataDiNascita").innerHTML = datadinascita;
+
+var luogoDiNascita = prompt("Inserisci il tuo luogo di nascita: ","");
+
+document.getElementById("luogoDiNascita").innerHTML = luogoDiNascita;
+
+var simboloZodiacale = prompt("Inserisci il tuo segno zodiacale: ","");
+
+document.getElementById("segno").innerHTML = simboloZodiacale;
+
